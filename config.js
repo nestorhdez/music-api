2b4257e9d1ed2844c0593dc85e3084ab54eaecbe
Nestor hdez <nestorhdez.26@gmail.com>
2019-08-23 20:40:33 +0100

    Change port to heroku

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
     },
     development: {
         db: process.env.MONGODB_URI,
-        port: 3001,
+        port: process.env.PORT,
         SECRET_TOKEN: process.env.SECRET_TOKEN,
         SECRET_REFRESH_TOKEN: process.env.SECRET_REFRESH_TOKEN,
         GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
